101cb265143f14d96d8d9ee85aeca658512c4758
Mukul Sharma <mukulsh29@gmail.com>
2022-07-16 16:13:51 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+const express		= require('express');
+const bodyParser	= require('body-parser');
+const path			= require('path');
+
+const routes 		= require('./routes/routes');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+app.use(routes);
+
+app.listen(3030);
